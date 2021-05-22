f0b421228710b2454b0f9d227788a60e845cf16b
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-05-22 02:50:04 -0700

    Assignment 2

---
+++ Assignment2/VSM.py
@@ -0,0 +1,19 @@
+import parsing
+import read_index
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+termIndex, termInfo, docIndex = parsing.Tokens()
+
+if (len(sys.argv) == 3):
+    queryFile = sys.argv[1]
+    resultsFile = sys.argv[2]
+
+def tfidf(docs, query):
+    vectorizer = TfidfVectorizer()
+    docTransform = vectorizer.fit_transform(docs)
+
+    query_tfids = vectorizer.transform([query])
+    cosineSimilarity = cosine_similarity(query_tfids, docTransform).flatten()
+
+    return cosineSimilarity
