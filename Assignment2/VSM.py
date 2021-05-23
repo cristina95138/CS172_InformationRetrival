c385cac9bfd9e9aae246f8bfd0362d88dc6d594d
Cristina Lawson <30380172+cristina95138@users.noreply.github.com>
2021-05-23 02:14:47 -0700

    Assignment 2

--- Assignment2/VSM.py
+++ Assignment2/VSM.py
@@ -1,35 +1,65 @@
 import parsing
-import read_index
+#import read_index
+#import sys
+import string
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
+from nltk.stem.lancaster import LancasterStemmer
+
+lancaster = LancasterStemmer()
 
 termIndex, termInfo, docIndex = parsing.Tokens()
 
-if (len(sys.argv) == 3):
-    queryFile = sys.argv[1]
-    resultsFile = sys.argv[2]
+queries = []
+splitQueries = []
 
-def tfidf(docs, query):
-    vectorizer = TfidfVectorizer()
-    docTransform = vectorizer.fit_transform(docs)
+file = open('query_list.txt', 'r')
+fileLines = file.readlines()
 
-    query_tfids = vectorizer.transform([query])
-    cosineSimilarity = cosine_similarity(query_tfids, docTransform).flatten()
+for line in fileLines:
+    line = line.strip()
+    queries.append(line)
 
-    return cosineSimilarity
+for query in queries:
+    query = query.replace('.', '')
+    query = query.replace('"', '')
+    query = query.replace(',', '')
+    query = query.split()
+
+    for i in range(len(query)):
+        query[i] = lancaster.stem(query[i])
+
+    splitQueries.append(query)
+
+for query in splitQueries:
+    queryNum = query[0]
+
+    for word in query[1:]:
+        print(word)
+        if(termInfo.get(word) != None):
+            print('Number of documents containing term: ', termInfo.get(word).get('numDocs'))
+            print('\n')
+
+#if (len(sys.argv) == 3):
+#    queryFile = sys.argv[1]
+#    resultsFile = sys.argv[2]
+
+
+
+# def tfidf(docs, query):
+#     vectorizer = TfidfVectorizer()
+#     docTransform = vectorizer.fit_transform(docs)
+#
+#     query_tfids = vectorizer.transform([query])
+#     cosineSimilarity = cosine_similarity(query_tfids, docTransform).flatten()
+#
+#     return cosineSimilarity
 
 #def file_len(file)
 #	with open(file) as f: 
 #		for i,l in enumerate(f);
 #			pass
 #	return i + 1
-   
-file1 = open('query_list.txt','r')
-for line in file1:
-	s = splice(4, len(line))
-	x = line[s].split()
-	for len(s)
-		#insert code to pass through assignment 1 and get docs for query
 
 
 
